9dd728b72cd735bd1e6129219958fbfb3199c9c9
kalaiarasy <skalaiarasy@gmail.com>
2021-05-11 20:45:48 -0400

    First commit

---
+++ MVCCoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCCoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Input()
+        {
+            return View();
+        }       
+
+        public IActionResult Output(CoffeeShop coffeeShop)
+        {
+            ViewData["FirstName"] = coffeeShop.FirstName;
+            ViewData["LastName"] = coffeeShop.LastName;
+            ViewData["Email"] = coffeeShop.Email;
+            ViewData["Password"] = coffeeShop.Password;
+            ViewData["Venilla"] = coffeeShop.Venilla;
+            ViewData["Mocha"] = coffeeShop.Mocha;
+            ViewData["Caramel"] = coffeeShop.Cramel;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCCoffeeShop/Models/CoffeeShop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MVCCoffeeShop.Models
+{
+    public class CoffeeShop
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Password { get; set; }
+        public string Flavors { get; set; }
+        public string Venilla { get; set; }
+        public string Mocha { get; set; }
+        public string Cramel { get; set; }
+        
+
+    }
+}
